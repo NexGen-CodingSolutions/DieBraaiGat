62eab96f12e747c23657918e4def7aa8858e5314
Zaid <zaidgarth19@gmail.com>
2025-01-24 20:42:17 +0200

    started with the home page

---
+++ braaiGat/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+
+export default function Navbar(params) {
+    return (
+        
+
+<nav className="bg-white relative w-full ">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="/HomePage/Images/logo.png" className="h-28" alt="Company Logo"/>
+  </a>
+  <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <a href="">
+      <i className="fa-solid fa-cart-shopping text-red text-4xl px-4 py-2 mr-10"></i>
+      </a>
+      <button type="button" className="text-white font-rubik bg-red rounded-lg text-base px-4 py-2 text-center ">Sign Up</button>
+      <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:text-brown focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+  </div>
+  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
+      <li>
+        <a href="#" className=" block text-2xl py-2 px-3 text-brown">Catalogue</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 text-2xl px-3 text-brown rounded">Location</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 text-2xl px-3 text-brown rounded">Services</a>
+      </li>
+      <li>
+        <a href="#" className="block py-2 text-2xl px-3 text-brown rounded">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+    )
+}
---
+++ braaiGat/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,49 @@
+import Navbar from "../../components/Navbar/Navbar";
+import  './HomePage.css'
+
+export default function HomePage(params) {
+    return (
+        <>
+        <Navbar/>
+        <div className=" bg-blueack w-full min-h-screen flex pl-6 pt-7 pr-6 pb-7">
+            <div className="bg-[url('./HomePage/Images/heroSection.jpg')] bg-cover w-full flex justify-center items-center opacity-85">
+                <p className="font-rubik text-white font-boldt text-3xl w-3xl">🔥 Savor the Flavor: Your Ultimate BBQ Experience Starts Here! </p>
+            </div>
+        </div>
+        <div className='item-section'>
+            <div className='items'>
+                <div className="items-section">
+                <div className="item  flex-col justify-start items-end ">
+                <img src="./HomePage/Images/BraaiStand.png" alt=""  />
+                <div className="item-description  bg-red font-[Oswald] text-2xl text-white p-2" ><p>Portable Adjustable Braai Stand</p></div>
+                </div>
+                <div className="item flex-col justify-start items-end ">
+                <img src="./HomePage/Images/KettleBraai.png" alt="" />
+                <div className="item-description  bg-red font-[Oswald] text-2xl text-white p-2" ><p>Big5 Enamel Kettle Braai </p></div>
+                </div>
+                </div>
+                <div className=" w-6xl flex-col  justify-center items-end ">
+                <img src="./HomePage/Images/Jetmaster.png" className="w-full" alt=""  />
+                <div className=" bg-red font-[Oswald] text-2xl text-white p-2  rounded-br-lg rounded-bl-lg" ><p>Jetmaster Potjie Cooker </p></div>
+                
+                </div>
+            </div>
+            <div className="">
+                <h1 className="font-rubik text-5xl mb-3">Our Catalogue</h1>
+                <p className="font-[Oswald] text-3xl leading-12  ">Explore our top-notch BBQ equipment and discover the tools for grilling perfection! From reliable smokers to versatile grills, every piece is designed to elevate your BBQ game. Gear up and master the art of outdoor cooking</p>
+                <button type="button" className="text-white font-rubik bg-red rounded-lg text-base px-4 py-4 mt-5 text-center ">View Equipment</button>
+            </div>
+        </div>
+
+        <div className="spice-add w-full bg-semi-black relative p-10 mt-10 ">
+            <div className="spice-add-caption bg-brown absolute w-2xl h-96 flex-col items-center p-20 justify-center rounded-lg left-30 top-20 ">
+                <h1 className="font-rubik text-3xl text-white">Grill Gear & Sauce</h1>
+                <p className="font-[Oswald] text-lg text-white leading-10">Equip your grill station with The Braai Gat’s premium BBQ essentials!<br/> Explore our collection of signature sauces, rubs, and top-notch grilling equipment</p>
+                <button type="button" className="text-white font-rubik bg-red rounded-lg text-base px-4 py-4 mt-5 text-center ">Shop Now</button>
+            </div>
+            <img src="./HomePage/Images/MerchSauce.jpg" alt="" className="w-4xl h-[438px] ml-auto"  />
+        </div>
+        </>
+        
+    )
+}
